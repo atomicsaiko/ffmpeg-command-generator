1e17b3a53bd8660f8500235d2c84f8930d114bcc
atomicsaiko <atomicsaiko@gmail.com>
2017-11-08 15:34:58 +0100

    Add copy FFmpeg command line button

--- src/components/FFmpegOutputCode.js
+++ src/components/FFmpegOutputCode.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class FFmpegOutputCommand extends Component {
   render() {
@@ -14,13 +15,16 @@ class FFmpegOutputCommand extends Component {
     return (
       <div>
         <p>FFmpeg generated command line to run: </p>
-         <textarea
-           rows='4'
-           cols='50'
-           style={styles.textarea}
-           value={this.props.command}
-           placeholder={`ffmpeg -i "input.mov" "output.mxf"`}>
-         </textarea>
+          <textarea
+            rows='4'
+            cols='50'
+            style={styles.textarea}
+            value={this.props.command}
+            placeholder={`ffmpeg -i "input.mov" "output.mxf"`}>
+          </textarea>
+          <CopyToClipboard text={this.props.command}>
+            <button>Copy</button>
+          </CopyToClipboard>
          <p>Download FFmpeg binary <a href='http://ffmpeg.org/download.html'>here</a>.</p>
       </div>
     )
